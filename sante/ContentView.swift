0530143f8c9f772a78374f65ebaf26fce7b2f6ab
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-23 19:01:15 +0100

    arriere plan en noir (debut du debut )

--- sante/ContentView.swift
+++ sante/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        HStack {
+            Color(.black)
+          
+            
+            
+            
+            
+            
+            
+            
+        }// HSATCK
+    }// BODY
+}// STRUCT
 
 #Preview {
     ContentView()
