c1575dcc99be77d6db7f2a68ec2423c9ee788a7f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-24 01:57:55 +0100

    fin

--- sante/ContentView.swift
+++ sante/ContentView.swift
@@ -8,101 +8,154 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var progress1 = 0.8
-    @State private var progress2 = 0.8
+    @State private var progress1 = 0.2
+    @State private var progress2 = 0.5
     @State private var progress3 = 0.8
 
 
     var body: some View {
-        VStack {
-            ZStack{
-//                Color(.black)
+        
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea() // Ignore les zones sûres pour couvrir toute la zone
 
-                Circle()
-                    .stroke(lineWidth: 40.0)
-                    .frame(width: 330, height: 330)
-                Circle()
-                    .trim(from: 0.0 , to: progress1)
-                    .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
-                    .foregroundColor(.red)
-                    .frame(width: 330, height: 330)
-
-
-                
-                
-                
-                
-                Circle()
-                    .stroke(lineWidth: 40.0)
-                    .frame(width: 240, height: 290)
-                Circle()
-                    .trim(from: 0.0 , to: progress2)
-                    .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
-                    .foregroundColor(.green)
-                    .frame(width: 240, height: 290)
-
-
-                
-                
-                
-                
-                Circle()
-                    .stroke(lineWidth: 40.0)
-                    .frame(width: 150, height: 290)
-                Circle()
-                    .trim(from: 0.0 , to: progress3)
-                    .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
-                    .foregroundColor(.blue)
-                    .frame(width: 150, height: 290)
-
-            }// FIN ZSTACK
-            .padding(40)
             
-                Text("bouger")              // texteetxtextetxtextetx
-            
-                HStack{
-                    Text("valeur: \(Int(progress1 * 100))")
-                    Slider(value: $progress1, in: 0...1 )
-                        .accentColor(.red)
+            VStack {
+                        ZStack{
+                            Image(systemName: "bolt.heart.fill")
+                                .font(.system(size: 55))
+                                .foregroundColor(.cyan)
+
+
+                            Circle()
+                                .stroke(lineWidth: 40.0)
+                                .frame(width: 330, height: 330)
+                            Circle()
+                                .trim(from: 0.0 , to: progress1)
+                                .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
+                                .foregroundColor(.red)
+                                .frame(width: 330, height: 330)
+
+
+                            
+                            
+                            
+                            
+                            Circle()
+                                .stroke(lineWidth: 40.0)
+                                .frame(width: 240, height: 290)
+                            Circle()
+                                .trim(from: 0.0 , to: progress2)
+                                .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
+                                .foregroundColor(.green)
+                                .frame(width: 240, height: 290)
+
+
+                            
+                            
+                            
+                            
+                            Circle()
+                                .stroke(lineWidth: 40.0)
+                                .frame(width: 150, height: 290)
+                            Circle()
+                                .trim(from: 0.0 , to: progress3)
+                                .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
+                                .foregroundColor(.blue)
+                                .frame(width: 150, height: 290)
+                                .padding(80)
+
+
+                        }// FIN ZSTACK
+                        .padding(40)
+                
+                
+                
+                
+                
+                
+                        
+                            Text("Bouger")              // texteetxtextetxtextetx
+                    .padding(.bottom, -10)
+
+                    .foregroundStyle(.white)
+
+                        
+                            HStack{
+                                Text("valeur: \(Int(progress1 * 100))")
+                                    .foregroundStyle(.white)
+                                    .padding()
+
+
+                                Slider(value: $progress1, in: 0...1 )
+                                    .accentColor(.red)
+                                    .padding()
+
+
+                                
+                                
+                        }// FIN HSTACK
+                        
+                        
+                        Text("Entrainer")                //  ttxxetxtetxetxetxtetx
+                    .padding(.bottom, -20)
+                    .foregroundStyle(.white)
 
                     
+                        HStack{
+                            Text("Valeur: \(Int(progress2 * 100))")
+                                .foregroundStyle(.white)
+                                .padding()
+
+
+                            Slider(value: $progress2, in: 0...1 )
+                                .accentColor(.green)
+                                .padding()
+
+
+
+                    }// FIN HSTACK
+                        
+                        
+                        
+                        Text("Lever")                   // textextettxextetxte
+                    .padding(.bottom, -20)
+                    .foregroundStyle(.white)
+
                     
-            }// FIN HSTACK
+                        HStack{
+                            Text("valeur: \(Int(progress3 * 100))")
+                                .foregroundStyle(.white)
+                                .padding()
+
+                            Slider(value: $progress3)
+                                .accentColor(.blue)
+                                .padding()
+
+
+
+                    }// FIN HSTACK
+                        
+                        
+                      
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                    }// HSATCK
             
             
-            Text("entrainer")                //  ttxxetxtetxetxetxtetx
+            
+            
+            
+            
+        } // FIN ZZZZZZZ
+
         
-            HStack{
-                Text("valeur: \(Int(progress2 * 100))")
-                Slider(value: $progress2, in: 0...1 )
-                    .accentColor(.green)
-
-
-        }// FIN HSTACK
-            
-            
-            
-            Text("lever")                   // textextettxextetxte
-        
-            HStack{
-                Text("valeur: \(Int(progress3 * 100))")
-                Slider(value: $progress3)
-                    .accentColor(.blue)
-
-
-        }// FIN HSTACK
-            
-            
-          
-            
-            
-            
-            
-            
-            
-            
-        }// HSATCK
-        .padding()
+        .ignoresSafeArea(.all)
     }// BODY
 }// STRUCT
 
